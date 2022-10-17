25d1cb79773848f695007361e1ab150cf766d91d
nodnarb12500 <nodnarb12500@airmail.cc>
2022-10-17 18:45:39 -0400

    added an HTML page and made it accessable

--- index.js
+++ index.js
@@ -3,17 +3,15 @@ const express = require("express");
 const app = express();
 const db = require("./db/database");
 
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use("/resources", express.static("resources"));
 app.set("/resources", express.static("resources"));
 
-app.get("/", (req, res) => {
-  // send file instead of static?
-  // make static a resources folder instead and manually send HTMl files?
-});
 
+/* Pages for modifying shit */
 app.post("/db/create", async (req, res) => {
   console.log(req.body);
   const results = await db.createAudio(req.body);
@@ -29,11 +27,11 @@ app.post("/db/modify/:id", async (req, res) => {
 });
 
 /* You might want to leave this commented out */
-app.get("/db/rm/:id", async (req, res) => {
-  // mark stuff for delete and hide instead?
-  await db.deleteAudio(req.params.id);
-  res.status(200).json({success: true});
-});
+// app.get("/db/rm/:id", async (req, res) => {
+//   // mark stuff for delete and hide instead?
+//   await db.deleteAudio(req.params.id);
+//   res.status(200).json({success: true});
+// });
 
 app.get("/db/get/:id", async (req, res) => {
   /* Get id and send back the JSON */
@@ -42,4 +40,9 @@ app.get("/db/get/:id", async (req, res) => {
 
 });
 
-app.listen(3000, () => console.log("server is listening on port 3000"));
+/* user interactable pages */
+app.get("/", (req, res) => {
+  res.sendFile('html/index.html', {root: __dirname});
+});
+
+app.listen(3000, () => console.log("server is listening on port 3069"));
