f21d11c528a7961192fd0492f20cf76ad3d1c883
nodnarb12500 <nodnarb12500@airmail.cc>
2022-10-22 20:26:13 -0400

    moved some stuff into their own function and added volume controls using the scroll wheel

--- resources/js/media.js
+++ resources/js/media.js
@@ -41,7 +41,6 @@ function loadMedia(data) {
   /* Parse data from server and loop though it */
   // turn the json array into HTML
   var json = data;
-
   // clear out any todo things
   // mediaInfo.innerText = "";
 
@@ -57,83 +56,90 @@ function loadMedia(data) {
       /* Object prop is what i wanted i can now start using the ojects to create html */
       var audio = JSON.parse(JSON.stringify(obj[prop]));
 
-      let info = document.createElement("div")
-      let name = document.createElement("p");
-      let length = document.createElement("p");
-
-      let progressBar = document.createElement("div");
-
-      let waveform = document.createElement("img");
-      let audioFile = document.createElement("audio");
-
-      let extraInfo = "";
-      if (audio.artist) {
-        console.log("artist found")
-        extraInfo = extraInfo.concat("<br>Artist: " + audio.artist);
-      }
-      if (audio.album) {
-        console.log("album found");
-        extraInfo = extraInfo.concat("<br>Album: " + audio.album);
-      }
-
-      name.innerHTML = "Name: " + audio.name + extraInfo;
-
-      name.className = "leftInfo";
-
-      /* somehow convert the likes into stars out of 5 */
-      let starRating = "";
-
-      if (audio.rating != "-1") {
-        for (let i = parseInt(audio.rating); i != 0; i--) {
-          starRating = starRating.concat("&starf;");
-        };
-        /* fill in the rest of the stars with empty ones */
-        for (let i = 5 - parseInt(audio.rating); i != 0; i--) {
-          starRating = starRating.concat("&star;");
-        }
-
-      } else {
-        for (let i = 5; i != 0; i--) {
-          starRating = starRating.concat("&star;");
-        }
-      }
-
-      length.innerHTML = "Length: " + audio.length + "<br />Rating: " + starRating;
-      length.className = "rightInfo";
-
-      progressBar.id = "progressBar";
-
-      waveform.src = "/resources/media/thumbs/" + audio.waveform;
-      waveform.id = "waveformImg";
-      waveform.draggable = false;
-
-      waveform.addEventListener("mousedown", waveformClick, false);
-
-      info.appendChild(progressBar);
-      info.appendChild(name);
-      info.appendChild(length);
-      info.appendChild(waveform);
-
-      info.className = "container";
-
-      mediaInfo.appendChild(info);
-
-      audioFile.id = "audioFile";
-      audioFile.src = "/resources/media/Music/" + audio.fileName;
-      audioFile.controls = true;
-
-      audioFile.ontimeupdate = function() {
-        waveformDisplay();
-      }
-
-      audioPlayer.appendChild(audioFile);
-
-      /* set the POST url for the rating form */
-      ratingForm.action = "/db/modify/" + audio.id;
+      loadInfo(audio);
 
     }
   }
 }
+
+function loadInfo(audio) {
+  /* shit here adds information to the mediaInfo Div */
+  let info = document.createElement("div")
+  let name = document.createElement("p");
+  let length = document.createElement("p");
+
+  let progressBar = document.createElement("div");
+
+  let waveform = document.createElement("img");
+  let audioFile = document.createElement("audio");
+
+  let extraInfo = "";
+  if (audio.artist) {
+    console.log("artist found")
+    extraInfo = extraInfo.concat("<br>Artist: " + audio.artist);
+  }
+  if (audio.album) {
+    console.log("album found");
+    extraInfo = extraInfo.concat("<br>Album: " + audio.album);
+  }
+
+  name.innerHTML = "Name: " + audio.name + extraInfo;
+
+  name.className = "leftInfo";
+
+  /* somehow convert the likes into stars out of 5 */
+  let starRating = "";
+
+  if (audio.rating != "-1") {
+    for (let i = parseInt(audio.rating); i != 0; i--) {
+      starRating = starRating.concat("&starf;");
+    };
+    /* fill in the rest of the stars with empty ones */
+    for (let i = 5 - parseInt(audio.rating); i != 0; i--) {
+      starRating = starRating.concat("&star;");
+    }
+
+  } else {
+    for (let i = 5; i != 0; i--) {
+      starRating = starRating.concat("&star;");
+    }
+  }
+
+  length.innerHTML = "Length: " + audio.length + "<br />Rating: " + starRating;
+  length.className = "rightInfo";
+
+  progressBar.id = "progressBar";
+
+  waveform.src = "/resources/media/thumbs/" + audio.waveform;
+  waveform.id = "waveformImg";
+  waveform.draggable = false;
+
+  waveform.addEventListener("mousedown", waveformClick, false);
+  waveform.addEventListener("wheel", scrollWheel, false);
+
+  info.appendChild(progressBar);
+  info.appendChild(name);
+  info.appendChild(length);
+  info.appendChild(waveform);
+
+  info.className = "container";
+
+  mediaInfo.appendChild(info);
+
+  audioFile.id = "audioFile";
+  audioFile.src = "/resources/media/Music/" + audio.fileName;
+  audioFile.controls = true;
+
+  audioFile.ontimeupdate = function() {
+    waveformDisplay();
+  }
+
+  audioPlayer.appendChild(audioFile);
+
+  /* set the POST url for the rating form */
+  ratingForm.action = "/db/modify/" + audio.id;
+}
+
 
 /* Variables */
 var pendingClick;
@@ -183,6 +189,35 @@ function waveformClick(e) {
   } else if (e.button == 2) {
     console.log("rightclick");
   }
+}
+
+/* Scroll wheel */
+function scrollWheel(e) {
+
+  let currentVolume = document.getElementById("audioFile").volume;
+
+  if (event.deltaY < 0) {
+    // Volume Up
+    if (currentVolume == 1) {
+      // stop errors by making it not execute the thing
+    } else {
+      document.getElementById("audioFile").volume = currentVolume + 0.05;
+    }
+
+  }
+  else if (event.deltaY > 0) {
+    // Volume Down
+    if (currentVolume == 0) {
+      // Stop execution when volume is down
+    } else {
+      document.getElementById("audioFile").volume = currentVolume - 0.05;
+
+    }
+
+  }
+
+  console.log(currentVolume);
+
 }
 
 function prependChecks(a) {
