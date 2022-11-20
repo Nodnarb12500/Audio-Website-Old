94feb677bd10a19cc75275e6e4824d6223c3b0a5
nodnarb12500 <nodnarb12500@airmail.cc>
2022-11-20 17:28:37 -0500

    when one song ends next one begins

--- resources/js/playlist.js
+++ resources/js/playlist.js
@@ -21,18 +21,29 @@ function storePlaylist(id) {
     console.log(id);
 
     localStorage.setItem("playlist", JSON.stringify(playList));
-    if (arrayIndex == null) {
-        arrayIndex = 0;
-    }
+    if (arrayIndex == null) { arrayIndex = 0; }
     localStorage.setItem("arrayIndex", arrayIndex);
 
 }
 
-function removePlaylist(id, index) {
+function removePlaylistItem(id) {
     // remove items from the playlist and update the local storage variable including the index
     // need the index? or can i easily find the index? or can i just cut it out with the thing?
 
 
+    console.log(playList.indexOf(id));
+
+    playList.splice(playList.indexOf(id), 1);
+
+    /* save the new array after */
+    localStorage.setItem("playlist", JSON.stringify(playList));
+    if (arrayIndex == null) { arrayIndex = 0; }
+    localStorage.setItem("arrayIndex", arrayIndex);
+
+}
+
+function movePlaylistItems() {
+    // figure out how the hell to move shit
 
 }
 
@@ -41,15 +52,79 @@ function clearPlaylist() {
     localStorage.clear();
 }
 
+function addListener() {
+    let audioPlayer = document.getElementById("audioFile");
+    // store and grab a variable 
+
+    if (localStorage.getItem("playing") == "yes") { 
+        document.getElementById("audioFile").play();
+        if (!document.getElementById("audioFile").paused) {
+            document.getElementById("playIcon").style.display = "none";
+        }
+    }
+
+    audioPlayer.addEventListener("play", (e) => {
+        // set localstorage thing to playing and if playing exists then start playing when the next audio exists
+        localStorage.setItem("playing", "yes");
+    }, false);
+
+    audioPlayer.addEventListener("paused", (e) => {
+        localStorage.setItem("playing", null);
+        console.log("paused");
+    }, false);
+
+    audioPlayer.addEventListener("ended", (e) => {
+        console.log("++");
+        arrayIndex++;
+        localStorage.setItem("arrayIndex", arrayIndex);
+        getPlaylistItem();
+    });
+
+}
+
 function getPlaylistPreview() {
     // a loop that goes though the entire array and creates a preview for each item and appends it to the playlist overflow box
-
-    let row = document.createElement("div");
-    let name = document.createElement("p");
-    let waveformImg = docuemnt.createElement("img");
+    url = window.location.href.split('/');
 
     for (let i = 0; i < playList.length; i++) {
-    
+        let search = url[0] + "//" + url[2] + "/db/get/" + playList[i];
+
+        let row = document.createElement("div");
+        let name = document.createElement("p");
+        let waveformImg = document.createElement("img");
+        let deleteBtn = document.createElement("button");
+
+        searchMedia(search).then(result => {
+            var json = JSON.parse(result);
+            for (var key in json) {
+                if (!json.hasOwnProperty(key)) continue;
+                var obj = json[key];
+                for (var prop in obj) {
+                    if (!obj.hasOwnProperty(prop)) continue;
+        
+                    /* Object prop is what i wanted i can now start using the ojects to create html */
+                    var audio = JSON.parse(JSON.stringify(obj[prop]));
+
+                    let extraInfo = "";
+                    if (audio.artist) { extraInfo = extraInfo.concat("<br>Artist: " + audio.artist); }
+                    if (audio.album) { extraInfo = extraInfo.concat("<br>Album: " + audio.album); }
+                  
+                    name.innerHTML = "Name: " + audio.name + extraInfo;
+                    waveformImg.src = "/resources/media/thumbs/" + audio.waveform;
+            
+                    deleteBtn.innerText = "remove";
+                    deleteBtn.addEventListener("click", (e) => { removePlaylistItem(audio.id); }, false);
+
+                    row.className = "playlistItem";
+
+                    row.appendChild(waveformImg);
+                    row.appendChild(name);
+                    row.appendChild(deleteBtn);
+                    
+                    document.getElementById("playlist").appendChild(row);
+                }
+            }
+        });
     }
 }
 
@@ -59,25 +134,16 @@ function getPlaylist() {
         playList = JSON.parse(localStorage.getItem("playlist"));
         arrayIndex = parseInt(localStorage.getItem("arrayIndex"));
 
+    } else { console.log("no stored values"); }
 
-        console.log("Grabbed the Stored values, playlist: " + playList + " index: " + arrayIndex);
-        console.log("current song is: " + playList[arrayIndex]);
-
-        /* here place all the loop to go though for running getPlaylistPreview() */
-
-    } else { 
-        console.log("no stored values");
-    }
 }
 
-function getPlaylistItem() {
+async function getPlaylistItem() {
     // give the waveform player the information it needs
     // hopfully the screen clears and doesnt leave shit in ram? lmao
 
     url = window.location.href.split('/');
     let search = url[0] + "//" + url[2] + "/db/get/" + playList[arrayIndex];
-
-    console.log(search);
 
     // maybe merge this with get media?
     searchMedia(search).then(result => {
